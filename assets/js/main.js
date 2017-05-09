6881ff532acdffad1b3a2817a8d5f99980fa899d
genarvaez <ge_naravez@hotmail.com>
2017-05-09 13:33:44 -0300

    funcionalidad boton agregar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+
 function Student(id,studentname,techpoints,lifepoints,status){
 	this.id = id;
 	this.studentname = studentname;
@@ -8,8 +9,20 @@ function Student(id,studentname,techpoints,lifepoints,status){
 // elementos a agregar
 //const student01 = new Student(1,"Susana",80,80,"Active");
 
+
+
+function pedirDatos(datos)
+{
+	return prompt("Ingrese el " + datos + " de la estudiante")
+}
+
 res = [];
+function addStudent()
+{
+	var nombre = pedirDatos("nombre");
+	var notaT = pedirDatos("Nota Tecnica")
+	var notaE = pedirDatos("Nota socio-Emocional");
+	res.push( new Student(res.length +1, nombre, notaT, notaE, "active"))
+	return res;
+}
 
-res.push(student01);
-
-//console.log(res);
