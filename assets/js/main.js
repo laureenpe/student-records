0957359b202d95500411357f7746a7da04bd02a8
camila-frangp <camila.frangp@gmail.com>
2017-05-09 15:35:39 -0300

    Agregando imagenes en foma aleatoria

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,8 +8,47 @@ function Student(id,studentname,techpoints,lifepoints,status){
 // elementos a agregar
 //const student01 = new Student(1,"Susana",80,80,"Active");
 
+function ver_imagen()
+{
+  var n=0;
+  this[n++]="imagenes/1.png";
+  this[n++]="imagenes/2.png";
+  this[n++]="imagenes/3.png";
+  this[n++]="imagenes/4.png";
+  this[n++]="imagenes/5.png";
+  this.N=n;
+}
+var imagen=new ver_imagen();
+src= imagen[ Math.floor(Math.random() * imagen.N) ]
+
+function pedirDatos(datos)
+{
+	return prompt("Ingrese el " + datos + " de la estudiante")
+}
+
 res = [];
+var print = document.getElementById("print")
 
-res.push(student01);
+function addStudent()
+{
+	var nombre = pedirDatos("nombre");
+	var notaT = pedirDatos("Nota Tecnica")
+	var notaE = pedirDatos("Nota socio-Emocional");
+	res.push( new Student(res.length +1, nombre, notaT, notaE, "active"))
+	return res;
+}
 
-//console.log(res);
+function showStudent(){
+printHTML(" ");
+
+res.forEach(function(elemento){
+print.innerHTML += "<img src="+src+">" + "<br><strong>" + "Nombre: " + "</strong>" + elemento.studentname + "<br>" + elemento.techpoints + "<br>" 
++ elemento.lifepoints + "<br>" + elemento.status + "<br>"
+})
+
+}
+
+function printHTML (html){
+ print.innerHTML = '';
+ print.innerHTML = html;
+}
